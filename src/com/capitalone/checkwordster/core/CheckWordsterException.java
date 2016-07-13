d3a91a5df9eb418507c82e75d24836748e107816
Howard Deiner <howard.deiner@deinersoft.com>
2016-07-13 17:33:48 -0500

    Initial commit

---
+++ src/com/capitalone/checkwordster/core/CheckWordsterException.java
@@ -0,0 +1,6 @@
+package com.capitalone.checkwordster.core;
+
+public class CheckWordsterException extends Exception {
+    public CheckWordsterException() { super(); }
+    public CheckWordsterException(String message) { super(message); }
+}
